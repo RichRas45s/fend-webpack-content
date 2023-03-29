bbbd79b29b0925e7d0006e1122a6a9cead0728c4
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-03-29 14:48:57 -0400

    alert added

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,6 @@ import { handleSubmit } from './js/formHandler'
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I MAN Ras Tayo will be Webdev serving U !")
+alert('Me say U cyan touch dis ah Bwoy yah!')
 console.log("CHANGE!!");
