c655645c8dd63b335495e591ed9da24aee837f79
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-04-11 00:30:32 -0400

    added const

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,14 +2,17 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
+       output: {
+         libraryTarget: 'var',
+         library: 'Client'
+     },
     module: {
         rules: [
             {
@@ -20,14 +23,19 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: ['style-loader','css-loader','sass-loader']
-            }
-        ]
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+        ],
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 }
 
