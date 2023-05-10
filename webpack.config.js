56c32f0a2df37100ea7dfad4ea0e8b49b90a2cd9
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-05-10 17:39:20 -0400

    add webpack config test

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const webpack = require('webpack')
+
+
+module.exports = {
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    stats: 'verbose',
+    output:{
+        libraryTarget:'var',
+        library: 'Client'
+    },
+
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader','css-loader','sass-loader']
+            }
+            
+        ]
+    },
+
+}
