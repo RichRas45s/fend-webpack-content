03824c1faebef8fee898b334369f978e65ab7d77
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-05-10 20:21:45 -0400

    new webpk dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const { merge } = require('webpack-merge')
-const StylesLoader = require('styles-loader')
-const stylesLoader = new StylesLoader()
+
 
 module.exports = {
     entry: './src/client/index.js',
-    mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
     output:{
@@ -34,15 +30,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
         })
+        
     ]
 }
+
+
