94006e721fbd4849f3f7bae46edda7e8baec5307
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-05-13 21:28:25 -0400

    1 commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,6 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-const cors = require('cors');
-app.use(cors());
 
 const app = express()
 
@@ -12,11 +10,12 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('Hey Richard listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
