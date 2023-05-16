8fe797562c013f6b8e541ef7b30a3d44b0396809
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-05-16 14:05:07 -0400

    added dot env

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,24 @@
+// Setup empty JS object to act as endpoint for all routes
+// projectData = {};
+const dotenv = require('dotenv');
+require('dotenv').config();
+console.log(`Yo tAYO yur API key is ${process.env.API_KEY}`);
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const cors = require('cors')
 
 const app = express()
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+// const cors= require('cors');
+// app.use(cors());
 
 app.use(express.static('dist'))
 
@@ -15,9 +31,12 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Hey Richard listening on port 8081!')
+    console.log('Hey Tayo listening on port 8081!')
 })
-
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+// const textapi = meaning({
+   // application_id: process.env.API_ID,
+   // application_key: process.env.API_KEY
+// });
