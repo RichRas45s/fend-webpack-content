3acf15b90bb45a719430aac22c5aff3b5ae0adec
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-05-19 20:27:59 -0400

    formhand comit const data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,34 +2,41 @@ const url="https://api.meaningcloud.com/sentiment-2.1"
 
 function handleSubmit(event) {
    // check what text was put into the form field
-  let formText = document.getElementById('name').value
+  let formText = document.getElementById('url').value
   Client.checkForName(formText)
   console.log("::: Form Submitted :::")
    
   const formEl = document.querySelector('.form');
   formEl.addEventListener('submit', event => {
   event.preventDefault();
-  const formdata = new FormData(formEl);
+  const formData = new FormData(formEl);
+  const data = Object.fromEntries (formData)
+  console.log(formData);
 
-  formdata.append("key", process.env.API_KEY);
-  formdata.append("txt", "YOUR TEXT HERE");
-  formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+  formData.append("key", process.env.API_KEY);
+  formData.append("txt", "YOUR TEXT HERE");
+  formData.append("lang", "en");  // 2-letter code, like en es fr ...
 
-  const requestOptions = {
+const requestOptions = {
     method: 'POST',
-    body: formdata,
+    body: formData,
     redirect: 'follow'
   };
-  
   const response = fetch(url, requestOptions)
   .then(response => ({
     status: response.status, 
-    body: response.json()
-  }))
+    body: response.json(data)
+ }))
   .then(({ status, body }) => console.log(status, body))
-  .catch(error => console.log('error', error));
+  
+  //.then (res => res.json())
+  //.then (data => console.log(data))
+    .catch(error => console.log('error', error));
 
 })}
+
   
+
+
 export { handleSubmit }
 
