6770736d476809cd8772db05495f2b8858f0865f
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-05-21 14:37:23 -0400

    formhand load status not pend

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-const url="https://api.meaningcloud.com/sentiment-2.1"
+//const url="https://api.meaningcloud.com/sentiment-2.1"
 
 function handleSubmit(event) {
    // check what text was put into the form field
@@ -13,6 +13,9 @@ function handleSubmit(event) {
   const data = Object.fromEntries (formData)
   console.log(formData);
 
+  //loadData ('/add',{ agreement: data.agreement, subjectivity: data.subjectivity, confidence: data.confidence})
+  //updateUI()
+
   formData.append("key", process.env.API_KEY);
   formData.append("txt", "YOUR TEXT HERE");
   formData.append("lang", "en");  // 2-letter code, like en es fr ...
@@ -22,17 +25,50 @@ const requestOptions = {
     body: formData,
     redirect: 'follow'
   };
+/*
   const response = fetch(url, requestOptions)
   .then(response => ({
     status: response.status, 
     body: response.json()
   }))
   .then(({ status, body }) => console.log(status, body))
-  .catch(error => console.log('error', error));
+  .catch(error => console.log('error', error))  */
 
-
+const loadData =async() => {
+  try { 
+  const url= "https://api.meaningcloud.com/sentiment-2.1";
+  const response = await fetch(url, requestOptions)
+  const data = await response.json()
+  return data;
+  } catch (err) {
+  console.log('error');
+  }
+  };
+  loadData().then((data) => console.log(data));
 
 })}
+
+
+
+
+
+async function updateUI() {
+  const request = await fetch('/all');
+  try {
+    // transform into JSON
+    const allData = await request.json();
+    console.log(allData);
+    // Write updated data to DOM elements
+    document.getElementById('results').innerHTML = allData.result;
+
+
+
+
+  } catch (error) {
+    console.log('error', error);
+    //appropriately handle the error
+  }
+}
 
   export { handleSubmit }
 
