50244fc01ff06117db6b1af95f3b4b1ba6bcc457
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-06-06 11:36:57 -0400

    port changed

--- src/server/index.js
+++ src/server/index.js
@@ -37,8 +37,8 @@ function getData (req, res) {
 }  */
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Hey Tayo listening on port 8081!')
+app.listen(8080, function () {
+    console.log('Hey Tayo listening on port 8080!')
 })
 
 
