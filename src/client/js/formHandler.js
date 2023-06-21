34c69d83405df7b122d23b08eb482e01de4490f7
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-06-21 19:47:49 -0400

    All 1st commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,102 +1,58 @@
-const url="https://api.meaningcloud.com/sentiment-2.1"
 function handleSubmit(event) {
    // check what text was put into the form field
-  const formText = document.getElementById('url').value
-  Client.checkForName(formText)
-  console.log("::: Form Submitted :::")
+  // const formText = document.getElementById('url').value
+  // Client.checkForName(formText)
+  // console.log("::: Form Submitted :::")
    
   const formEl = document.querySelector('.form');
   formEl.addEventListener('submit', event => {
   event.preventDefault();
-  const formData = new FormData(formEl);
-  const data = Object.fromEntries (formData)
-  // console.log(formData);
   const inputText= document.getElementById('text').value;
   // inputText.innerHTML=('results');
-
-  formData.append("key", `${process.env.API_KEY}`);
-  formData.append("txt", inputText);
-  formData.append("lang", "en");  // 2-letter code, like en es fr ...
-
-const requestOptions = {
-    method: 'POST',
-    body: formData,
-    redirect: 'follow'
-  };
-
-  const response = fetch(url, requestOptions)
-  .then((response) => {
-    if (response.ok) {
-    return response.json(); 
-    }else {
-      throw new Error ('NETWORK RESPONSE NOT OK');
-    }
   })
-  .then(function(data) {
-    console.log(data);
-   displaySentiments(data);
-  })
-  //.then(function(data) {
-   // document.getElementById('results').innerHTML = data.message
-
-  .catch((error) => {
-    console.error("FETCH ERROR:", error);
-  });
+  const formText = document.getElementById('url').value
+  if (Client.checkForName(formText)) {
+    fetch("http://localhost:8081/api", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ url: formText })
+    }).then(res => res.json())
+        .then(function (res) {
+            document.getElementById('score_tag').innerHTML = "Score Tag : " + res.score_tag
+            document.getElementById('confidence').innerHTML = "Agreement : " + res.confidence
+            document.getElementById('agreement').innerHTML = "Confidence : " + res.agreement
+            document.getElementById('subjectivity').innerHTML = "Subjectivity : " + res.subjectivity
+            document.getElementById('resultSection').className = ""
+        })
+}}
   
- // document.getElementById('agreement').innerHTML=data[0]
-  
-  function displaySentiments(data)  {
-  const agreement= data.agreement;
-  const agreeDiv= document.getElementById('agreement');
-  agreeDiv.innerHTML= agreement;
-  const confidence = data.confidence;
-  const confiDiv = document.getElementById('confidence');
-  confiDiv.innerHTML=confidence;
-  const irony= data.irony;
-  const ironDiv= document.getElementById('irony');
-  ironDiv.innerHTML=irony;   
-  const model= data.model;
-  const modelDiv=document.getElementById('model');
-  modelDiv.innerHTML=model;  
-  const score= data.score_tag;
-  const scoreDiv=document.getElementById('score_tag');
-  scoreDiv.innerHTML=score; 
-  const subject= data.subjectivity;
-  const subDiv=document.getElementById('subjectivity');
-  subDiv.innerHTML=subject; 
-  const text=data.text;
-  const textDiv=document.getElementById('text');
-  textDiv.innerHTML=text;
+//   function displaySentiments(data)  {
+//   const agreement= data.agreement;
+//   const agreeDiv= document.getElementById('agreement');
+//   agreeDiv.innerHTML= agreement;
+//   const confidence = data.confidence;
+//   const confiDiv = document.getElementById('confidence');
+//   confiDiv.innerHTML=confidence;
+//   const irony= data.irony;
+//   const ironDiv= document.getElementById('irony');
+//   ironDiv.innerHTML=irony;   
+//   const model= data.model;
+//   const modelDiv=document.getElementById('model');
+//   modelDiv.innerHTML=model;  
+//   const score= data.score_tag;
+//   const scoreDiv=document.getElementById('score_tag');
+//   scoreDiv.innerHTML=score; 
+//   const subject= data.subjectivity;
+//   const subDiv=document.getElementById('subjectivity');
+//   subDiv.innerHTML=subject; 
+//   const text=data.text;
+//   const textDiv=document.getElementById('text');
+//   textDiv.innerHTML=text;
+// }
+// })}
 
-
-
- 
-  }
-
-
-
-
-
-  /*
-const loadData =async() => {
-  try { 
-  const url= "https://api.meaningcloud.com/sentiment-2.1";
-  const response = await fetch(url, requestOptions)
-  const data = await response.json()
-  return data;
-  } catch (err) {
-  console.log('error');
-  }
-  };
-  loadData().then((data) => console.log(data));
-*/
-})}
-
-
-
-
-
-
-
-  export { handleSubmit }
+export { handleSubmit }
 
--- src/server/index.js
+++ src/server/index.js
@@ -5,46 +5,67 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors')
-
 const app = express()
+// const Formadata = require('form-data');
+// const fetch = require('node-fetch');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+//const { response } = require('express');
+const { request } = require('http');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
-// const cors= require('cors');
-// app.use(cors());
 
-app.use(express.static('dist'))
+app.use(cors());
+
+app.use(express.static('dist'));
+app.use(express.json({limit: '1mb'}))
 
 console.log(__dirname)
+console.log(JSON.stringify(mockAPIResponse))
 
  app.get('/', function (req, res) {
     res.sendFile('dist/index.html') 
     // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-/* app.get('/all', getData);
- 
-function getData (req, res) {
-    res.send(projectData);
-    console.log(projectData);
-}  */
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Hey Tayo listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Hey Richard listening on port 8081!')
+})
+// app.get('/api', function (req, res) {
+//    res.send(mockAPIResponse)
+//    console.log(mockAPIResponse);
+// })
+
+
+const apiURL ="https://api.meaningcloud.com/sentiment-2.1"
+const apiKey = `${process.env.API_KEY}`
+
+
+app.post('/api', async(req,res)=> {
+    const url = req.body.url;
+    let formDataInfo = new Formadata();
+    formDataInfo.append("key", `${process.env.API_KEY}`);
+    formDataInfo.append("txt", inputText);
+    formDataInfo.append("lang", "en"); // 2-letter code, like en es fr ...
+    
+    const response = await fetch(`${apiURL}key=${apiKey}&url=${url}&lang=en`, { method: "POST", body: formDataInfo})
+    const apiResponse = await response.text()
+    if (apiResponse && apiResponse.status.code == 0)
+        res.send(apiResponse)
+    else res.status(500).send({ message: 'error' , error: error })
 })
 
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)  
-})
+
+
+
 
 
 process.env.API_KEY
-console.log(`Your API key is ${process.env.API_KEY}`);
+// console.log(`Your API key is ${process.env.API_KEY}`);
 
