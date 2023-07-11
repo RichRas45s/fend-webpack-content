c9cc38ca5eaf0702b3f67d6ebc97c03c3e454627
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-07-11 18:50:12 -0400

    july form Commit 2

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,42 +1,60 @@
  function handleSubmit(event) {
     event.preventDefault();
-    
-  const formText = document.getElementById('name').value
-    
+    const formText = document.getElementById('name').value
   if (Client.checkForName(formText)) {
-      console.log("::: Form Submitted :::")
-
-  const formEl = document.querySelector('.form');
-  formEl.addEventListener('submit', event => {
-  const formData = new FormData(formEl);
-  const data = Object.fromEntries (formData)
-    // console.log(formData);
-  
-  const inputText= document.getElementById('text').value;
+  console.log("::: Form Submitted :::")
+    
+  const formEl = document.querySelector('.form') 
+ 
+   formEl.addEventListener('submit', async event => {
+      const formData = new FormData(formEl);
+      const data = Object.fromEntries (formData)
+        // console.log(formData);
+      const inputText= document.getElementById('text').value;
   // inputText.innerHTML=('results');
  
   // formData.append("key", `${process.env.API_KEY}`);
-  formData.append("txt", inputText);
-  formData.append("lang", "en");  // 2-letter code, like en es fr ...
+     formData.append("txt", inputText);
+     formData.append("lang", "en");  // 2-letter code, like en es fr ...
 
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.strigify(formData),
-    redirect: 'follow'
-  };
+  // const requestOptions = {
+  //   method: 'POST',
+  //   headers: {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.strigify(formData),
+   
+  // };
  
-  const formText = document.getElementById('name').value
 
-   fetch("http://localhost:8081/sentiment", requestOptions)
 
-  });
-    throw new Error ('NETWORK RESPONSE NOT OK');
-  }
-}
+   await fetch('http://localhost:8081/sentiment') 
+    .then(res => res.json())
+    .then(function(res) {
+      console.log(res);
+      displaySentiments(res);
+        document.getElementById('results').innerHTML = res.results
+    })
+
+
+  // .then((response) => {
+  //   if (response.ok) {
+  //   return response.json(); 
+  //   }else {
+  //     throw new Error ('NETWORK RESPONSE NOT OK');
+  //   }
+  // })
+  // .then(function(data) {
+  //   console.log(data);
+  // //  displaySentiments(data);
+  // })
+
+
+  .catch((error) => {
+   console.log('not Available young lord');
+  })   
+},
 
   
       
@@ -53,30 +71,30 @@
 
   // )}
   
-//   function displaySentiments(data)  {
-//   const agreement= data.agreement;
-//   const agreeDiv= document.getElementById('agreement');
-//   agreeDiv.innerHTML= agreement;
-//   const confidence = data.confidence;
-//   const confiDiv = document.getElementById('confidence');
-//   confiDiv.innerHTML=confidence;
-//   const irony= data.irony;
-//   const ironDiv= document.getElementById('irony');
-//   ironDiv.innerHTML=irony;   
-//   const model= data.model;
-//   const modelDiv=document.getElementById('model');
-//   modelDiv.innerHTML=model;  
-//   const score= data.score_tag;
-//   const scoreDiv=document.getElementById('score_tag');
-//   scoreDiv.innerHTML=score; 
-//   const subject= data.subjectivity;
-//   const subDiv=document.getElementById('subjectivity');
-//   subDiv.innerHTML=subject; 
-//   const text=data.text;
-//   const textDiv=document.getElementById('text');
-//   textDiv.innerHTML=text;
-// }
-// })}
+  function displaySentiments(data)  {
+  const agreement= data.agreement;
+  const agreeDiv= document.getElementById('agreement');
+  agreeDiv.innerHTML= agreement;
+  const confidence = data.confidence;
+  const confiDiv = document.getElementById('confidence');
+  confiDiv.innerHTML=confidence;
+  const irony= data.irony;
+  const ironDiv= document.getElementById('irony');
+  ironDiv.innerHTML=irony;   
+  const model= data.model;
+  const modelDiv=document.getElementById('model');
+  modelDiv.innerHTML=model;  
+  const score= data.score_tag;
+  const scoreDiv=document.getElementById('score_tag');
+  scoreDiv.innerHTML=score; 
+  const subject= data.subjectivity;
+  const subDiv=document.getElementById('subjectivity');
+  subDiv.innerHTML=subject; 
+  const text=data.text;
+  const textDiv=document.getElementById('text');
+  textDiv.innerHTML=text;
+}
+  )}}
 
 export { handleSubmit }
 
