d210bfd00a6499f7bcce1872b8d65adb3353cf3b
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-07-20 11:19:51 -0400

    july 20 2023 formhand commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -37,51 +37,7 @@
     })
 }
 
-//  await fetch('http://localhost:8081/sentiment') //requestOptions)
-//   .then(res => res.json())
-//   .then(function(res) {
-//       document.getElementById('results').innerHTML = res.message
-//   })
-// },
-  
-      
-      
-      
-      // .then(res => res.json())
-      // .then(function (data) {
-      //       document.getElementById('results').innerHTML = data.message
-      //       document.getElementById('agreement').innerHTML = "Agreement : " + data.agreement
-      //       document.getElementById('confidence').innerHTML = "Confidence : " + data.confidence
-           
-      //   })
-    // }}
 
-  // )}
-  
-//   function displaySentiments(data)  {
-//   const agreement= data.agreement;
-//   const agreeDiv= document.getElementById('agreement');
-//   agreeDiv.innerHTML= agreement;
-//   const confidence = data.confidence;
-//   const confiDiv = document.getElementById('confidence');
-//   confiDiv.innerHTML=confidence;
-//   const irony= data.irony;
-//   const ironDiv= document.getElementById('irony');
-//   ironDiv.innerHTML=irony;   
-//   const model= data.model;
-//   const modelDiv=document.getElementById('model');
-//   modelDiv.innerHTML=model;  
-//   const score= data.score_tag;
-//   const scoreDiv=document.getElementById('score_tag');
-//   scoreDiv.innerHTML=score; 
-//   const subject= data.subjectivity;
-//   const subDiv=document.getElementById('subjectivity');
-//   subDiv.innerHTML=subject; 
-//   const text=data.text;
-//   const textDiv=document.getElementById('text');
-//   textDiv.innerHTML=text;
-// }
-  // )}
 
 export { handleSubmit }
 
