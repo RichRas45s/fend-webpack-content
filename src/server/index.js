fc2447b84f3d92c0a6f86871e3fe03c94f804004
RichRas45s <107000837+RichRas45s@users.noreply.github.com>
2023-07-23 19:46:21 -0400

    JULY 23 iNDEX  SERVER COMMIT

--- src/server/index.js
+++ src/server/index.js
@@ -24,7 +24,8 @@ const bodyParser = require('body-parser');
 //const { response } = require('express');
 // const { request } = require('http');
 // const { response } = require('express');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -32,7 +33,7 @@ app.use(cors());
 
 app.use(express.static('dist'))
 app.use(express.json())
-app.use(express.json({limit: '1mb'}))
+app.use(express.json({limit: '10mb'}))
 
 // console.log(__dirname)
 // console.log(JSON.stringify(mockAPIResponse))
@@ -50,8 +51,8 @@ app.listen(8081, function () {
     console.log('Hey Richard listening on port 8081!')
 })
 
-// app.get('/sentiment', function (req, res) {
-//   res.send(mockAPIResponse)
+// app.post('/postload', function (req, res) {
+//   console.log(req.body);
 // })
 
 const apiKey = process.env.API_KEY
@@ -62,12 +63,11 @@ process.env.API_KEY
 
 
 app.get('/sentiment', async function (req, res){
-const url = req.body.url;
+  const text = req.body.text;
 const apiURL ="https://api.meaningcloud.com/sentiment-2.1"
 const formData = new FormData();
-//const inputText = document.getElementById('text').value;
 formData.append("key", `${process.env.API_KEY}`);
-formData.append("txt", "I love mondays");
+formData.append("txt", "I hate mondays");
 formData.append("lang", "en");  // 2-letter code, like en es fr ...
 
 
@@ -95,12 +95,8 @@ const response = await fetch(apiURL, requestOptions)
 
  });
 
-//  app.post('/sentiment', (request, respond) => {
-//   const url = request.body.url;
-//   let response = fetch(`${apiURL}key=${apiKey}&url=${url}&lang=en`, { method: "POST" })
-//   let articleResponse = response.json()
-//   if (articleResponse && articleResponse.status.code == 0)
-//     res.send(articleResponse)
-//   else res.status(500).send({ message: 'error' , error: error })
+//  app.post('/sentiment', function (req, res) {
+//   const text= req.body.text;
+//   console.log(text);
 // })
- 
+
